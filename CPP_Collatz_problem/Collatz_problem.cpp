7854528ef93a2aed757e8a8fcd2e40d35cf5637d
timapple-ai <58660268+timapple-ai@users.noreply.github.com>
2020-07-16 17:18:01 +0900

    add function: export result to txt file

---
+++ CPP_Collatz_problem/Collatz_problem.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#define F "test.txt"
+
+using namespace std;
+
+void showArray(vector<long> array)
+{
+    cout << array.size() << ": [";
+    for (int i = 0; i < array.size(); i++)
+    {
+        cout << array[i];
+        if (i != array.size() - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+}
+
+void exportImage(vector<long> array)
+{
+    ofstream outputfile(F);
+    outputfile << array.size() << ": [";
+    for (int i = 0; i < array.size(); i++)
+    {
+        outputfile << array[i];
+        if (i != array.size() - 1)
+        {
+            outputfile << ", ";
+        }
+    }
+    outputfile << "]" << endl;
+    outputfile.close();
+}
+
+int main()
+{
+    long n = 103693973672629630;
+    vector<long> array;
+    array.push_back(n);
+
+    //for (int i = 0; n != 1; i++)
+    //for (; n != 1;)
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        else
+        {
+            n = n * 3 + 1;
+        }
+        array.push_back(n);
+    }
+    showArray(array);
+    exportImage(array);
+    return 0;
+}
